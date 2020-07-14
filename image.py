60f97d0a247cf3fff002707f5e86a8e1cb43b6db
Maitri Patel <maitripatel@maitris-air.home>
2020-07-14 17:58:36 -0400

    image.py

--- image.py
+++ image.py
@@ -1,1 +1,14 @@
-backgroung = pygame.image.load('images/')
+background = pygame.image.load('assets/background.png').convert()
+#background = pygame.transform.scale2x(background)
+
+bomb = pygame.image.load('assets/bomb.png').convert()
+
+ground = pygame.image.load('assets/ground.png').convert()
+
+fly_dino1 = pygame.image.load('assets/1FlyDino.png').convert()
+
+fly_dino2 = pygame.image.load('assets/2FlyDino.png').convert()
+
+fly_dino3 = pygame.image.load('assets/3FlyDino.png').convert()
+
+fly_dino4 = pygame.image.load('assets/4FlyDino.png').convert()
