3882cb5c937728377dfbc1b7d3ad2a1d64664603
Maitri Patel <maitri.patel293@gmail.com>
2020-07-15 15:46:12 -0400

    try

--- main_game.py
+++ main_game.py
@@ -12,8 +12,17 @@ gravity = 0.75
 
 score = 0
 highScore = 0
-
 ground_x_pos = -50
+
+fly_dino_x_pos = 350
+flyDino_height = [200, 350, 400]
+
+
+def create_flyDino():
+    random_flyDino_pos = random.choice(flyDino_height)
+    flyDino = img.fly_dino1
+
+
 
 #GAME LOOP
 while True:
@@ -28,15 +37,15 @@ while True:
                 dinoMovement = 0
                 dinoMovement = -12
 
-    
 
-
-    
     screen.blit(imag.background, (0,0))
     ground_x_pos -= 2
     screen.blit(imag.ground, (ground_x_pos, 720))
+
     # screen.blit(imag.bomb, (400, 900))
-    # screen.blit(imag.fly_dino1, (350, 450))
+    fly_dino_x_pos -= 1
+    screen.blit(imag.fly_dino1, (fly_dino_x_pos, 450))
+    screen.blit(imag.fly_dino1, (fly_dino_x_pos + 567, 450))
     # screen.blit(imag.dinoMain, (10, 800))
 
     pygame.display.update()
--- methods.py
+++ methods.py
@@ -1,2 +1,6 @@
 def draw_ground():
-    
+
+def move_dino():
+    fly_dino_x_pos -= 2
+    screen.blit(imag.fly_dino1, (fly_dino_x_pos, 450))
+    screen.blit(imag.fly_dino1, (fly_dino_x_pos + 567, 450))
