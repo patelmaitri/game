d10cf9cdf9f8f6eeb6231f9d76a8a02705114b30
Praful Chunchu <prafulchunchu@prafuls-mbp.home>
2020-07-15 16:51:40 -0400

    updating Branch....

--- main_game.py
+++ main_game.py
@@ -10,6 +10,22 @@ def draw_ground():
     screen.blit(imag.ground, (ground_x_pos + 576, 720))
 
 
+def create_bomb():
+    new_bomb = imag.bomb.get_rect(midbottom = (585, 1000))
+    return new_bomb
+
+def bomb_movment(bombs):
+    for bomb in bombs:
+        bomb.centerx -= 5
+    return bombs
+
+def draw_bombs(bombs):
+    for bomb in bombs:
+        screen.blit(imag.bomb, bomb)
+
+
+
+
 
 screen = pygame.display.set_mode((576, 1024), pygame.RESIZABLE)
 clock = pygame.time.Clock()
@@ -23,6 +39,10 @@ highScore = 0
 
 ground_x_pos = -50
 
+bomb_list = []
+SPAWNBOMB = pygame.USEREVENT
+pygame.time.set_timer(SPAWNBOMB, 4500)
+
 #GAME LOOP
 while True:
     #Event loop - NEEDED IN ANY GAME
@@ -35,14 +55,21 @@ while True:
             if event.key == pygame.K_SPACE:
                 dinoMovement = 0
                 dinoMovement = -12
+        if event.type == SPAWNBOMB:
+            bomb_list.append(create_bomb())
 
     
 
 
     
     screen.blit(imag.background, (0,0))
-    ground_x_pos -= 5
+    ground_x_pos -= 2
     draw_ground()
+    if ground_x_pos <= -576:
+        ground_x_pos = 0
+
+    bomb_list = bomb_movment(bomb_list)
+    draw_bombs(bomb_list)
     
     # screen.blit(imag.bomb, (400, 900))
     # screen.blit(imag.fly_dino1, (350, 450))
