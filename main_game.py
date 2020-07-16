bd6c38db96c6c04ebc34c894bc1fbb7fe2fe271a
Maitri Patel <maitri.patel293@gmail.com>
2020-07-15 22:41:49 -0400

    methods

--- main_game.py
+++ main_game.py
@@ -1,37 +1,47 @@
 import pygame, sys, random, time
-import image as imag
 
-def create_fly_dino():
-    random_fly_dion_pos = random.choice(fly_dino_height)
-    new_fly_dino = imag.fly_dino1.get_rect(midtop = (700, random_fly_dion_pos))
-    return new_fly_dino
+import images as imag
+import methods as method
 
-def move_fly_dino(fdinos):
-    for fdino in fdinos:
-        fdino.centerx -= 5
-    return fdinos
+pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 256)
 
-def draw_fly_dino(fdinos):
-    for fdino in fdinos:
-        screen.blit(imag.fly_dino1, fdino)
+#game variables
+dinoMovement = 0
+gravity = 0.75
+
+#score
+score = 0
+highScore = 0
+
+#bomb
+bomb_list = []
+SPAWNBOMB = pygame.USEREVENT
+pygame.time.set_timer(SPAWNBOMB, 2000)
+
+#flying Dino
+fly_dino_list = []
+SPAWNFLYDINO = pygame.USEREVENT
+pygame.time.set_timer(SPAWNFLYDINO, 32000)
+
+#player dino
+DINOWALK = pygame.USEREVENT + 1
+pygame.time.set_timer(DINOWALK, 200)
+
+game_theme = pygame.mixer.music.load('nishat.mp3')
+time.sleep(2)
+pygame.mixer.music.play()
+
+# game_theme = pygame.mixer.music.load('Sounds/DinoTheme.mp3')
+# time.sleep(2)
+# pygame.mixer.music.play()
+
+
 
 pygame.init()
 
-screen = pygame.display.set_mode((576, 1024), pygame.RESIZABLE)
+# screen = pygame.display.set_mode((576, 1024), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 
-#Game variables
-dinoMovement = 0
-gravity = 0.75
-score = 0
-highScore = 0
-ground_x_pos =-50 
-
-#Flying Dino
-fly_dino_list = []
-SPAWNFLYDINO = pygame.USEREVENT
-pygame.time.set_timer(SPAWNFLYDINO, 2000)
-fly_dino_height = [200, 700, 800]
 
 #GAME LOOP
 while True:
@@ -44,21 +54,38 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 dinoMovement = 0
-                dinoMovement = -12
+                dinoMovement = -6
+
+        if event.type == DINOWALK:
+            if method.dino_index < 3:
+                method.dino_index += 1
+            else:
+                method.dino_index = 0
+
+            method.dino_surface, dino_rect = method.dino_animation()
+
+        if event.type == SPAWNBOMB:
+            bomb_list.append(method.create_bomb())
+
         #spawing dino
         if event.type == SPAWNFLYDINO:
-            fly_dino_list.append(create_fly_dino())
-    
-    #background
-    screen.blit(imag.background, (0,0))
-    
-    #flying dino 
-    fly_dino_list = move_fly_dino(fly_dino_list)
-    draw_fly_dino(fly_dino_list)
-   
-    #ground
-    ground_x_pos -= 2
-    screen.blit(imag.ground, (ground_x_pos, 500))
+            fly_dino_list.append(method.create_fly_dino())
+
+    imag.screen.blit(imag.background, (0,0))
+    method.ground_x_pos -= 2
+    #screen.blit(imag.ground, (ground_x_pos, 720))
+
+    method.draw_ground()
+
+    if method.ground_x_pos <= -679:
+        method.ground_x_pos = -50
+
+    imag.screen.blit(method.dino_surface, method.dino_rect)
+    bomb_list = method.bomb_movment(bomb_list)
+    method.draw_bombs(bomb_list)
+
+    fly_dino_list = method.move_fly_dino(fly_dino_list)
+    method.draw_fly_dino(fly_dino_list)
 
     pygame.display.update()
     clock.tick(120)
--- methods.py
+++ methods.py
@@ -1,4 +1,11 @@
-def draw_ground():
+import random
+
+import images as imag
+
+# fly_dino_list = []
+# SPAWNFLYDINO = pygame.USEREVENT
+# pygame.time.set_timer(SPAWNFLYDINO, 10000)
+fly_dino_height = [200, 700, 800]
 
 def create_fly_dino():
     random_fly_dion_pos = random.choice(fly_dino_height)
@@ -12,4 +19,41 @@ def move_fly_dino(fdinos):
 
 def draw_fly_dino(fdinos):
     for fdino in fdinos:
-        screen.blit(imag.fly_dino1, fdino)
+        imag.screen.blit(imag.fly_dino1, fdino)
+
+
+# bomb_list = []
+# SPAWNBOMB = pygame.USEREVENT
+# pygame.time.set_timer(SPAWNBOMB, 300)
+
+def create_bomb():
+    new_bomb = imag.bomb_img.get_rect(midbottom = (585, 500))
+    return new_bomb
+
+def bomb_movment(bombs):
+    for bomb in bombs:
+        bomb.centerx -= 5
+    return bombs
+
+def draw_bombs(bombs):
+    for bomb in bombs:
+        imag.screen.blit(imag.bomb_img, bomb)
+
+
+#ground
+ground_x_pos = -50
+
+def draw_ground():
+    #-50,720
+    imag.screen.blit(imag.ground, (ground_x_pos, 720))
+    imag.screen.blit(imag.ground, (ground_x_pos + 679, 720))
+
+dino_frames = [imag.dino1,imag.dino2,imag.dino3,imag.dino4]
+dino_index = 2
+dino_surface = dino_frames[dino_index]
+dino_rect = dino_surface.get_rect(center = (125,900))
+
+def dino_animation():
+    new_dino = dino_frames[dino_index]
+    new_dino_rect = new_dino.get_rect(center= (125, dino_rect.centery))
+    return new_dino,new_dino_rect
